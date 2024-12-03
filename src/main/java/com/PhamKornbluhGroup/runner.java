77ed8d3a34a809a6e90a1a5f3c32f9bb7d685c73
No Exploding Shadows <Daniel.Kornbluh23@gmail.com>
2024-12-03 05:27:21 -0600

    - Remove unneeded imports in main - Update pom.xml to produce FAT JAR with necessary dependencies all included

--- src/main/java/com/PhamKornbluhGroup/runner.java
+++ src/main/java/com/PhamKornbluhGroup/runner.java
@@ -1,9 +1,5 @@
 package com.PhamKornbluhGroup;
 
-import java.io.FileWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 public class runner {
     public static void main(String[] args) {
         new PublicStashTabBulkAPICaller().callAndPrintResults();
