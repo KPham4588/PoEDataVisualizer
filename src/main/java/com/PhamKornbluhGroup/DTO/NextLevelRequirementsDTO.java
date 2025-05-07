124de19816a603c7080608618453defcccea3290
No Exploding Shadows <Daniel.Kornbluh23@gmail.com>
2025-05-07 01:16:15 -0500

    Changes made after updates to the associated Dev Docs: (https://www.pathofexile.com/developer/docs/reference#type-Item)
    Specific Changes
    - Remove old DTOs
    
    Later Updates will aim to:
     - Rename new DTO2 package to DTO

--- src/main/java/com/PhamKornbluhGroup/DTO/NextLevelRequirementsDTO.java
+++
@@ -1,7 +0,0 @@
-package com.PhamKornbluhGroup.DTO;
-
-public class NextLevelRequirementsDTO {
-    private String name; // Ex: "Level"
-    private int[] values; // Ex: [ [ "35", 0 ] ]
-    private int displayMode; // Ex: 0
-}
--- src/main/java/com/PhamKornbluhGroup/DTO/RequirementsDTO.java
+++
@@ -1,8 +0,0 @@
-package com.PhamKornbluhGroup.DTO;
-
-public class RequirementsDTO {
-    private String name; // Ex: "level"
-    private String[] values; // Ex: "[ [ "59", 0 ] ]"
-    private int displayMode; // Ex: "0"
-    private int type; // Ex: "62" //p Note: Unsure whether this is an Optional Field
-}
