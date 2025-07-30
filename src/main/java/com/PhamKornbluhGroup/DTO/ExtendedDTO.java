bf026dcecb8633008bfef8ac6f5a924ed27e5113
No Exploding Shadows <Daniel.Kornbluh23@gmail.com>
2025-07-30 12:10:35 -0500

    - remove getSubcategoriesByExtendedId and simply set subcategory through a join in the getEntityById query - clean up comments

--- src/main/java/com/PhamKornbluhGroup/DTO/ExtendedDTO.java
+++ src/main/java/com/PhamKornbluhGroup/DTO/ExtendedDTO.java
@@ -1,26 +1,17 @@
 package com.PhamKornbluhGroup.DTO;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class ExtendedDTO {
     private int dbId;
     private int itemId;
-    private String category;        // Deprecated   // Optional
-    // this may need to become an Arraylist<String> to work
-//    private String[] subcategories; // Deprecated   // Optional
-    private ArrayList<String> subcategories; // Deprecated   // Optional
-    private long prefixes;                          // Optional
-    private long suffixes;                          // Optional
+    private String category;                 // Deprecated  // Optional
+    private ArrayList<String> subcategories; // Deprecated  // Optional
+    private long prefixes;                                  // Optional
+    private long suffixes;                                  // Optional
 
     @Override
     public String toString() {
-        System.out.println("Subcategories coming below");
-        for (String subcategories : subcategories) {
-            System.out.println(subcategories);
-        }
-        System.out.println("End of subcategories");
-        System.out.println();
         return "ExtendedDTO{" +
                 "dbId=" + dbId +
                 ", itemId=" + itemId +
