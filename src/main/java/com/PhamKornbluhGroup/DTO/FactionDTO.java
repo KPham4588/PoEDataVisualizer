a59a11d6e72f1cc26d8493c52d63a2b8b9e1fcb3
Khoa <15710620+KPham4588@users.noreply.github.com>
2025-08-12 22:37:14 -0500

    Made several new mappers and tested the FactionDTO to make sure that enums worked for insertion

--- src/main/java/com/PhamKornbluhGroup/DTO/FactionDTO.java
+++ src/main/java/com/PhamKornbluhGroup/DTO/FactionDTO.java
@@ -5,4 +5,36 @@ public class FactionDTO {
     private int logbookModsId;
     private FactionId factionId; // Called "faction" in Dev Docs - Faction1, Faction2, Faction3, or Faction4
     private String factionName;  // Called "name" in Dev Docs
+
+    public int getDbId() {
+        return dbId;
+    }
+
+    public void setDbId(int dbId) {
+        this.dbId = dbId;
+    }
+
+    public int getLogbookModsId() {
+        return logbookModsId;
+    }
+
+    public void setLogbookModsId(int logbookModsId) {
+        this.logbookModsId = logbookModsId;
+    }
+
+    public FactionId getFactionId() {
+        return factionId;
+    }
+
+    public void setFactionId(FactionId factionId) {
+        this.factionId = factionId;
+    }
+
+    public String getFactionName() {
+        return factionName;
+    }
+
+    public void setFactionName(String factionName) {
+        this.factionName = factionName;
+    }
 }
