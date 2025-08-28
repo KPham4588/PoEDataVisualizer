7fb9b48661c2aa97ec1e5f3d9ee7fba532793c0f
No Exploding Shadows <Daniel.Kornbluh23@gmail.com>
2025-08-27 21:30:57 -0500

    - Create first DAO test template - Add Junit dependency

---
+++ src/test/java/com/PhamKornbluhGroup/DAO/CrucibleDAOTest.java
@@ -0,0 +1,24 @@
+package com.PhamKornbluhGroup.DAO;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CrucibleDAOTest {
+
+    @Test
+    void getCrucibleById() {
+    }
+
+    @Test
+    void insertCrucible() {
+    }
+
+    @Test
+    void updateCrucible() {
+    }
+
+    @Test
+    void deleteCrucibleById() {
+    }
+}
