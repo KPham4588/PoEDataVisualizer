82d22079c3c658770200b50988adc548c3ad672e
No Exploding Shadows <Daniel.Kornbluh23@gmail.com>
2025-09-23 22:43:01 -0500

    - Create InfluencesDAO - Create Update Influences interface to include new method getEntityByItemId which is implemented in the resultmap

--- src/main/java/com/PhamKornbluhGroup/mybatismysqlimpl/IInfluencesDTO.java
+++ src/main/java/com/PhamKornbluhGroup/mybatismysqlimpl/IInfluencesDTO.java
@@ -3,4 +3,5 @@ package com.PhamKornbluhGroup.mybatismysqlimpl;
 import com.PhamKornbluhGroup.DTO.InfluencesDTO;
 
 public interface IInfluencesDTO extends IBaseDAO<InfluencesDTO> {
+    InfluencesDTO getEntityByItemId(int id);
 }
