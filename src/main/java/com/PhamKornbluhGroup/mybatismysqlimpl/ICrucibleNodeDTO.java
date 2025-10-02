fe11cbb462a01450e3d9641488f5795ad3ffb947
Khoa <15710620+KPham4588@users.noreply.github.com>
2025-10-01 21:40:34 -0500

    remove commented function name from crucible node interface

--- src/main/java/com/PhamKornbluhGroup/mybatismysqlimpl/ICrucibleNodeDTO.java
+++ src/main/java/com/PhamKornbluhGroup/mybatismysqlimpl/ICrucibleNodeDTO.java
@@ -3,6 +3,4 @@ package com.PhamKornbluhGroup.mybatismysqlimpl;
 import com.PhamKornbluhGroup.DTO.CrucibleNodeDTO;
 
 public interface ICrucibleNodeDTO extends IBaseDAO<CrucibleNodeDTO> {
-    // Keeping this within MyBatis Config, only able to be used within the parent Crucible ResultMap Query "getEntityById"
-    // List<CrucibleNodeDTO> getEntityByCrucibleId(int id);
 }
