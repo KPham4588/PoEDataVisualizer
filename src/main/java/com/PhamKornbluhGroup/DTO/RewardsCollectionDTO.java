1eabd3b8371b41c5a16e857010def6376896199b
Khoa <15710620+KPham4588@users.noreply.github.com>
2025-10-01 21:41:24 -0500

    create the rewards collection class, interface, DAO, and mapper

---
+++ src/main/java/com/PhamKornbluhGroup/DTO/RewardsCollectionDTO.java
@@ -0,0 +1,45 @@
+package com.PhamKornbluhGroup.DTO;
+
+import java.util.HashMap;
+
+public class RewardsCollectionDTO {
+    private int dbId;
+    private int rewardsId;
+    private String rewardsType;
+    private int rewardsAmount;
+
+    public RewardsCollectionDTO() {
+    }
+
+    public int getRewardsAmount() {
+        return rewardsAmount;
+    }
+
+    public void setRewardsAmount(int rewardsAmount) {
+        this.rewardsAmount = rewardsAmount;
+    }
+
+    public String getRewardsType() {
+        return rewardsType;
+    }
+
+    public void setRewardsType(String rewardsType) {
+        this.rewardsType = rewardsType;
+    }
+
+    public int getRewardsId() {
+        return rewardsId;
+    }
+
+    public void setRewardsId(int rewardsId) {
+        this.rewardsId = rewardsId;
+    }
+
+    public int getDbId() {
+        return dbId;
+    }
+
+    public void setDbId(int dbId) {
+        this.dbId = dbId;
+    }
+}
