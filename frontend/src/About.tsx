80c065a54475cddb7654f601151d807b168801d0
No Exploding Shadows <Daniel.Kornbluh23@gmail.com>
2025-10-15 06:42:37 -0500

    - Make header sit on-top - Darken image - Update parent dimensions, color, etc - Make children of "parent" scroll down instead of up - Set-up Scrollbar

--- frontend/src/About.tsx
+++ frontend/src/About.tsx
@@ -3,11 +3,313 @@ import './About.css';
 export default function About() {
     return(
         <div>
-            <p>About About About About About
+            <p className="body-block">About About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
+                About About About About About
+                About About About About
+                About About About About
+                About About AboutAbout About About About About
                 About About About About About
                 About About About About
                 About About About About
                 About About About </p>
+
+            <p className="body-block">sdasadsadsa dsadsadsads adsadsadsa dsadsadsad sadsadsadsa</p>
     </div>
 )
 }
