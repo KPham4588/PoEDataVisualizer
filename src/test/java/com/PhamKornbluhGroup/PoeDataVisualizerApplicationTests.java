50b67cfc3efaf998803d0e0fb31a326ebff67061
Khoa <15710620+KPham4588@users.noreply.github.com>
2025-11-25 22:56:37 -0600

    add new files for spring boot

--- src/test/java/com/PhamKornbluhGroup/PoeDataVisualizerApplicationTests.java
+++ src/test/java/com/PhamKornbluhGroup/PoeDataVisualizerApplicationTests.java
@@ -2,8 +2,10 @@ package com.PhamKornbluhGroup;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = runner.class)
 class PoeDataVisualizerApplicationTests {
 
 	@Test
