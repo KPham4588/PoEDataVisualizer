5d0dad2cfd8192d90fecedb3b2828bb9c4f368c9
No Exploding Shadows <Daniel.Kornbluh23@gmail.com>
2026-01-07 21:22:06 -0600

    - Update RewardsCollectionDeserializer to match expected structure of JSON

--- src/main/java/com/PhamKornbluhGroup/jsonParsing/RewardsCollectionDeserializer.java
+++ src/main/java/com/PhamKornbluhGroup/jsonParsing/RewardsCollectionDeserializer.java
@@ -19,90 +19,45 @@ public class RewardsCollectionDeserializer extends JsonDeserializer<List<Rewards
 
     @Override
     public List<RewardsCollectionDTO> deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
-        String lastKey = "";
-        JsonNode lastValueAsNode;
-        int lastValue = 0;
-
+        rewardsCollectionDeserializerLogger.error("Beginnning RewardsCollectionDeserializer deserialize method");
 
         ObjectMapper mapper = (ObjectMapper) p.getCodec();
         JsonNode node = mapper.readTree(p);
 
-        lastValueAsNode = node;
+        String lastKey = "";
+        int lastValue = 0;
+
 
         List<RewardsCollectionDTO> list = new ArrayList<>();
 
         if (node.isArray()) {
             rewardsCollectionDeserializerLogger.error("node is an array");
-            for (JsonNode child : node) {
-                Set<Map.Entry<String, JsonNode>> setOfRewardTypeAndAmount = child.properties();
-                for (Map.Entry<String, JsonNode> nextReward : setOfRewardTypeAndAmount) {
-                    RewardsCollectionDTO dto = new RewardsCollectionDTO();
-
-                    String key = nextReward.getKey();
-                    JsonNode valueAsNode = nextReward.getValue();
-
-                    //b Attempt #1 -- See if Get(key) works
-                    int value = valueAsNode.get(key).asInt();
-
-                    //b Attempt #2 -- See if asInt works
-//                    int value = valueAsNode.asInt();
-
-                    //b Attempt #3 -- See if get(0) works
-//                    int value = valueAsNode.get(0).asInt();
-
-                    dto.setRewardsType(key);
-                    dto.setRewardsAmount(value);
-                    list.add(dto);
-
-
-
-                    rewardsCollectionDeserializerLogger.error("Key = " + key);
-                    rewardsCollectionDeserializerLogger.error("valueAsNode = " + valueAsNode);
-                    rewardsCollectionDeserializerLogger.error("value = " + value);
-                    lastKey = key;
-                    lastValueAsNode = valueAsNode;
-                    lastValue = value;
-
-                }
-            }
+            throw new IOException("rewardsCollection should never be an Array. node is " + node.toPrettyString());
         }
         else if (node.isObject()) {
             rewardsCollectionDeserializerLogger.error("node is an object");
-            for (JsonNode child : node) {
-                Set<Map.Entry<String, JsonNode>> setOfRewardTypeAndAmount = child.properties();
-                for (Map.Entry<String, JsonNode> nextReward : setOfRewardTypeAndAmount) {
-                    RewardsCollectionDTO dto = new RewardsCollectionDTO();
 
-                    String key = nextReward.getKey();
-                    JsonNode valueAsNode = nextReward.getValue();
+            Set<Map.Entry<String, JsonNode>> setOfRewardTypeAndAmount = node.properties();
+            for (Map.Entry<String, JsonNode> nextReward : setOfRewardTypeAndAmount) {
+                RewardsCollectionDTO dto = new RewardsCollectionDTO();
 
-                    //b Attempt #1 -- See if Get(key) works
-                    int value = valueAsNode.get(key).asInt();
+                String key = nextReward.getKey();
+                int value = nextReward.getValue().asInt();
 
-                    //b Attempt #2 -- See if asInt works
-//                    int value = valueAsNode.asInt();
+                dto.setRewardsType(key);
+                dto.setRewardsAmount(value);
+                list.add(dto);
 
-                    //b Attempt #3 -- See if get(0) works
-//                    int value = valueAsNode.get(0).asInt();
+                rewardsCollectionDeserializerLogger.error("Key = " + key);
+                rewardsCollectionDeserializerLogger.error("value = " + value);
 
-                    dto.setRewardsType(key);
-                    dto.setRewardsAmount(value);
-                    list.add(dto);
-
-                    rewardsCollectionDeserializerLogger.error("Key = " + key);
-                    rewardsCollectionDeserializerLogger.error("valueAsNode = " + valueAsNode);
-                    rewardsCollectionDeserializerLogger.error("value = " + value);
-
-                    lastKey = key;
-                    lastValueAsNode = valueAsNode;
-                    lastValue = value;
-                }
+                lastKey = key;
+                lastValue = value;
             }
         }
 
         rewardsCollectionDeserializerLogger.error("we're at the end now");
         rewardsCollectionDeserializerLogger.error("Key = " + lastKey);
-        rewardsCollectionDeserializerLogger.error("valueAsNode = " + lastValueAsNode);
         rewardsCollectionDeserializerLogger.error("value = " + lastValue);
         return list;
     }
