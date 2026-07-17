256c4f70b8151970b5ca812fb6db6fa12e5e848e
No Exploding Shadows <Daniel.Kornbluh23@gmail.com>
2026-07-17 08:13:34 -0500

    - Temporarily use the "attributions" page to check out the data coming in from the "/getItemsAsChartDataByCustomParametersAsCostInChaos" endpoint

--- frontend/src/routes/Attributions.tsx
+++ frontend/src/routes/Attributions.tsx
@@ -1,13 +1,24 @@
 import '../css/Attributions.css';
 import {createFileRoute} from "@tanstack/react-router";
-import {useChartData} from "../hooks/fetchChartData.ts";
+import {useChartData} from "../hooks/fetchChartInfo.ts";
 
 function Attributions() {
     // const { data, isLoading, isError } = useChartData("Mageblood", "Keepers");
-    const { data, isLoading } = useChartData("Mageblood", "Keepers");
+    const { data, isLoading } = useChartData("Tabula Rasa", "Keepers");
     const placeHolder = isLoading ? "Loading..." : "Done Loading";
     // if (isLoading) return <p>Loading...</p>;
     // if (isError) return <p>Error loading data.</p>;
+    console.log("before data foreach in attributions")
+    console.log(data)
+    console.log("after logging initial \"data\" object")
+
+    data?.forEach((item) => {
+        console.log("Im in the for each loop")
+        console.log("item = " + item)
+        // children[children.length - 1] = item;
+    })
+
+    console.log("im in attributions")
     return (
         <div>
         <p className="attributions">
@@ -19,6 +30,7 @@ function Attributions() {
         </p>
             <p>{placeHolder}</p>
             <p>{JSON.stringify(data)}</p>
+            {/*<p>{children}</p>*/}
         </div>
         )
 }
