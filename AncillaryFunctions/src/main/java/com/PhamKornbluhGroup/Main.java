da8590989152abba83c4771a8057553b68b10611
Khoa Pham <15710620+KPham4588@users.noreply.github.com>
2026-08-04 22:31:14 -0500

    add code to ancillary functions main to insert into DB

--- AncillaryFunctions/src/main/java/com/PhamKornbluhGroup/Main.java
+++ AncillaryFunctions/src/main/java/com/PhamKornbluhGroup/Main.java
@@ -1,5 +1,9 @@
 package com.PhamKornbluhGroup;
 
+import com.PhamKornbluhGroup.DAO.ResultDAO;
+import com.PhamKornbluhGroup.DTO.ResultDTO;
+import com.PhamKornbluhGroup.jsonParsing.JSONParser;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -10,5 +14,17 @@ public class Main {
         APIResultData result = apiHandler.getPublicStashData("3176716451-3104538686-3029882391-3371404298-3262573054");
         System.out.println("Got back APIResultData");
         System.out.println(result.getResponseCode());
+
+        try {
+            ResultDTO resultDTO = JSONParser.parseAPIResultData(result);
+            System.out.println("Parsed APIResultData into ResultDTO");
+
+            ResultDAO resultDAO = new ResultDAO();
+            resultDAO.insertResult(resultDTO);
+            System.out.println("Inserted ResultDTO into the database");
+        }
+        catch (Exception e) {
+            System.out.println("Failure to parse and insert APIResultData. Message = " + e.getMessage());
+        }
     }
 }
